472ac6add239c592de284114b4ea9258323b2804
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-07-20 13:09:02 +0100

    spin the sloth machine

--- project.js
+++ project.js
@@ -6,20 +6,18 @@ const ROWS = 3;
 const COLS = 3;
 
 const SYMBOLS_COUNT = {
-    "A": 2,
-    "B": 4,
-    "C": 6,
-    "D": 8
-}
+  A: 2,
+  B: 4,
+  C: 6,
+  D: 8,
+};
 
 const SYMBOL_VALUES = {
-    "A": 5,
-    "B": 4,
-    "C": 3,
-    "D": 2
-}
-
-
+  A: 5,
+  B: 4,
+  C: 3,
+  D: 2,
+};
 
 const deposit = () => {
   while (true) {
@@ -52,27 +50,48 @@ const getNumberOfLines = () => {
 
 // 3. Collect a bet amount
 const getBet = (balance, lines) => {
-    while (true) {
-        const bet = prompt("Enter the bet per line: ");
-        const numberBet = parseFloat(bet);
-    
-        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
-          console.log("Invalid bet, try again");
-        } else {
-          return numberBet;
-        }
-      }
+  while (true) {
+    const bet = prompt("Enter the bet per line: ");
+    const numberBet = parseFloat(bet);
+
+    if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+      console.log("Invalid bet, try again");
+    } else {
+      return numberBet;
+    }
+  }
 };
 
-const spin = () => {
-    
-}
+// 4. spin the sloth machine
 
+const spin = () => {
+  const symbols = [];
+  for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+    for (let i = 0; i < count; i++) {
+      symbols.push(symbol);
+    }
+  }
+
+  const reels = [[], [], []];
+  for (let i = 0; i < COLS; i++) {
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
+  }
+
+  return reels;
+};
+
+const reels = spin();
+console.log(reels)
 let balance = deposit();
 const numberOfLines = getNumberOfLines();
 const bet = getBet(balance, numberOfLines);
 
-// 4. spin the sloth machine
 // 5. check if the user won
 // 6. give the user their winnings
 // 7. play again
