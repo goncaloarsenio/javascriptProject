48ebd168bf6ffa8881a7c30ca15826294ae19b33
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-07-20 11:38:33 +0100

    first commit

---
+++ project.js
@@ -0,0 +1,78 @@
+// 1. Deposit some money
+
+const prompt = require("prompt-sync")();
+
+const ROWS = 3;
+const COLS = 3;
+
+const SYMBOLS_COUNT = {
+    "A": 2,
+    "B": 4,
+    "C": 6,
+    "D": 8
+}
+
+const SYMBOL_VALUES = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+
+
+const deposit = () => {
+  while (true) {
+    // while -> vai verificar se o deposit é valido, caso não seja, vai continuar a pedir ao user para por o "deposit" corretamente, e só aí para o loop
+
+    const depositAmount = prompt("Enter a deposit amount: ");
+    const numberDepositAmount = parseFloat(depositAmount);
+
+    if (isNaN(numberDepositAmount) || numberDepositAmount <= 0) {
+      console.log("Invalid deposit amount, try again");
+    } else {
+      return numberDepositAmount;
+    }
+  }
+};
+// 2. Determine the number of lines to bet on
+
+const getNumberOfLines = () => {
+  while (true) {
+    const lines = prompt("Enter the number of lines to bet on (1-3): ");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Invalid number of lines, try again");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+// 3. Collect a bet amount
+const getBet = (balance, lines) => {
+    while (true) {
+        const bet = prompt("Enter the bet per line: ");
+        const numberBet = parseFloat(bet);
+    
+        if (isNaN(numberBet) || numberBet <= 0 || numberBet > balance / lines) {
+          console.log("Invalid bet, try again");
+        } else {
+          return numberBet;
+        }
+      }
+};
+
+const spin = () => {
+    
+}
+
+let balance = deposit();
+const numberOfLines = getNumberOfLines();
+const bet = getBet(balance, numberOfLines);
+
+// 4. spin the sloth machine
+// 5. check if the user won
+// 6. give the user their winnings
+// 7. play again
