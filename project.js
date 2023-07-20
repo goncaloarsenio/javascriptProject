4d38535f99939283e3512fd5c7fdb3f3977c44c7
goncaloarsenio <goncalo.arsenio92@gmail.com>
2023-07-20 15:15:53 +0100

    slot machine done

--- project.js
+++ project.js
@@ -72,8 +72,9 @@ const spin = () => {
     }
   }
 
-  const reels = [[], [], []];
+  const reels = [];
   for (let i = 0; i < COLS; i++) {
+    reels.push([]);
     const reelSymbols = [...symbols];
     for (let j = 0; j < ROWS; j++) {
       const randomIndex = Math.floor(Math.random() * reelSymbols.length);
@@ -86,12 +87,89 @@ const spin = () => {
   return reels;
 };
 
-const reels = spin();
-console.log(reels)
-let balance = deposit();
-const numberOfLines = getNumberOfLines();
-const bet = getBet(balance, numberOfLines);
+const transpose = (reels) => {
+  const rows = [];
+
+  for (let i = 0; i < ROWS; i++) {
+    rows.push([]);
+    for (let j = 0; j < COLS; j++) {
+      rows[i].push(reels[j][i]);
+    }
+  }
+  return rows;
+};
+
+const printRows = (rows) => {
+  //we are going to every single row in our rows, that´s gonna givr us an array representing the elements in that row
+
+  for (const row of rows) {
+    let rowString = "";
+
+    //we are going to loop trough the index and the element (symbol) that exists in this row. ie: if we have ["A","B","C"], it will  give -> index:0 element:A, index:1 element:B, index:2 element:C
+
+    for (const [i, symbol] of row.entries()) {
+      rowString += symbol;
+
+      //checking if it is the index of the last element? if it isn´t, we put the pipe "|", if it is, we don't
+
+      if (i != row.length - 1) {
+        rowString += " | ";
+      }
+    }
+    console.log(rowString);
+  }
+};
 
 // 5. check if the user won
+
+const getWinnings = (rows, bet, lines) => {
+  let winnings = 0;
+
+  for (let row = 0; row < lines; row++) {
+    const symbols = rows[row];
+    let allSame = true;
+
+    for (const symbol of symbols) {
+      if (symbol != symbols[0]) {
+        allSame = false;
+        break;
+      }
+    }
+
+    if (allSame) {
+      winnings += bet * SYMBOL_VALUES[symbols[0]];
+    }
+  }
+  return winnings;
+};
+
 // 6. give the user their winnings
+const game = () => {
+  let balance = deposit();
+
+  while (true) {
+    console.log("You have a balance of € " + balance);
+    const numberOfLines = getNumberOfLines();
+    const bet = getBet(balance, numberOfLines);
+    balance -= bet * numberOfLines;
+    const reels = spin();
+    const rows = transpose(reels);
+    printRows(rows);
+    const winnings = getWinnings(rows, bet, numberOfLines);
+    balance += winnings;
+    console.log("You won, € " + winnings.toString());
+
+    if (balance <= 0) {
+      console.log("You ran out of money");
+      break;
+    }
+
+    const playAgain = prompt("Do you want to playagain? (y/n) ");
+
+    if (playAgain != "y") break;
+  }
+};
+
+game();
+
 // 7. play again
